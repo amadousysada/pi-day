5e639137d3834211c3197c2612af5e21a61f0331
amadou sy <amsada93@gmail.com>
2023-03-14 03:50:48 +0100

    hey

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+extern crate js_sys;
+extern crate web_sys;
+
+mod utils;
+
+use rand::{thread_rng, Rng};
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, wasm-template-rust!");
+}
+
+#[wasm_bindgen]
+pub fn compute_pi(n: i32) -> f64{
+    let mut count: i32 = 0;
+    let mut random = thread_rng();
+
+    for _i in 0..n{
+        let x: f64 = random.gen::<f64>();
+        let y: f64 = random.gen::<f64>();
+        if x * x + y * y <= 1.0{
+            count += 1;
+        }
+    }
+    4.0*(count as f64 / n as f64)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ www/index.js
@@ -0,0 +1,12 @@
+import * as wasm from "monte-carlo-pi";
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+var result = document.getElementById("result");
+result.innerHTML = wasm.compute_pi(slider.value)
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  result.innerHTML = wasm.compute_pi(this.value)
+}
